d7cbccbf537822e8aef8c1fe4323beafcf3ec865
deglenn <65422462+deglenn@users.noreply.github.com>
2024-02-14 18:15:57 +1300

    Create Export_Data.js

---
+++ Export_Data.js
@@ -0,0 +1,27 @@
+// Export the image to an Google Drive.
+function rasterExport(layer, name) {
+  Export.image.toDrive({
+    image: layer,
+    description: 'SingleRasterToDrive',
+    fileNamePrefix: name,
+    region: ee.Geometry.BBox(88.5, 26.5, 92.5, 28.5), //Change to match your region!!!
+    crs: 'EPSG:4326',
+  });
+} 
+
+
+// Save the image to an Earth Engine asset.
+function save_REMO(layer, name) {
+  Export.image.toAsset({
+    image: layer,
+    description: 'SingleRasterToAsset',
+    assetId: name, 
+    region: ee.Geometry.BBox(88.5, 26.5, 92.5, 28.5), //Change to match your region!!!
+    crs: 'EPSG:4326',
+    pyramidingPolicy: {
+      'b4_mean': 'mean',
+      'b4_sample': 'sample',
+      'b4_max': 'max'
+    }
+  });
+}
